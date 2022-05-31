bc2ef6ce23d13f60c6424d6494423fd1511c5951
jpmeraz <84814896+jpmeraz@users.noreply.github.com>
2022-05-31 14:04:08 -0500

    Update and rename f1017beuler.m to main.m

--- main.m
+++ main.m
@@ -6,23 +6,25 @@ clc
 %Variables
 M = 0.02 % Kg
 R = 100 % Ohms
-L = 0.3 % metros
+L = 0.3 % Metros
 B = 2.5 % Teslas
-tf = 15;
+tf = 15; % Segundos
 
 %Intervalos de t
 n = 1000;
 
 %Valores iniciales
-t0 = 0;
-x0 = 0; %Corriente 1
+t0 = 0; % Segundos
+x0 = 0; %Posición Inicial m
 y0 = 0.2; %Velocidad Inicial ms^-2
 
-%Función
+% Y Corresponde a la velocidad  
+  
+%Funciónes
 f = @(t,x,y) y;
 g = @(t,x,y) -(((B^2)*(L^2)*y)/(R*M));
 
-%Llamado de función
+%Llamado de función Runge Kutta
 [t,x,y] = rungekuttadoble(f, g, t0, tf, x0, y0, n)
 
 figure(1)
